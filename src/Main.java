e55be5a14308a8e3224b8f3c6b94c4fa120d0484
Sosnus <sosnus15@gmail.com>
2019-05-15 14:26:53 +0200

    print informations about user 'sosnus'

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Nazwa grupy: ZPI_2019_Dzienni_IO2_2 \nTeamLeaderID: Rahly - tester ");
+        System.out.println("Operations: Sosnus");
     }
 }
