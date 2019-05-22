9529780ea90763ce9f828bc95d7b12e1f345f386
209424 <209424@edu.p.lodz.pl>
2019-05-22 15:06:51 +0200

    First comment
    (cherry picked from commit 53506ed)

--- src/IArithmeticsAdd.java
+++ src/IArithmeticsAdd.java
@@ -1,7 +1,5 @@
 public interface IArithmeticsAdd
 {
-    //First comment created by Rahly
     // Comment 1 209424
-    //First comment created by skydriver95
     double Addition(double A, double B);
 }
