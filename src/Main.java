6be119e8d06870488bedaf32875e5f924fb1916c
209424 <209424@edu.p.lodz.pl>
2019-05-22 15:23:08 +0200

    Revert "Zad 6 comment"
    This reverts commit 28a1bfbc9ac50f7577ffea29301410fb469697ae.
    
    Reverted to previous version

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Nazwa grupy: ZPI_2019_Dzienni_IO2_2 \nTeamLeaderID: Rahly - tester ");
-        System.out.println("Developer - 209424"); // information about 209424
+        System.out.println("Developer - 209424");
         System.out.println("Operations: Sosnus");
         System.out.println("Tester: Rahly");
     }
