0a0b183fde838699f8cf9fecdef92050fe3be5cb
209457 <209457@edu.p.lodz.pl>
2019-06-05 13:40:35 +0200

    Cleaned project and converted to Maven

--- src/IArithmeticsDiff.java
+++
@@ -1,8 +0,0 @@
-//Comment created by Rahly
- // Diff com
- // Comment 2 209424
-public interface IArithmeticsDiff
-{
-    //Second comment by skydriver95
-    double Difference(double A, double B);
-}
